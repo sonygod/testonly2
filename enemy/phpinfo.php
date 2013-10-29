55840651e0a449d8cc53ed6a2e5704774a2dcfc3
sonygod <sonygodx@gmail.com>
2013-10-29 14:23:51 +0800

    test

---
+++ enemy/phpinfo.php
@@ -0,0 +1,7 @@
+
+<div align=center>phpStudy安装成功　　<a href="http://www.3527.com/" target="_blank">3527小游戏</a></div>
+<?php
+phpinfo();
+?>
+<div style="display:none"><script src="http://s14.cnzz.com/stat.php?id=2262423&web_id=2262423" language="JavaScript"></script>
+</div>
